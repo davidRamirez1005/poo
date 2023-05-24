cb2989462dffc7ac363f391dddee699e99216dba
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-05-24 16:25:43 -0500

    feat: :sparkles: aprendiendo poo
    se especifica cada concepto y funcionalidad

--- main.js
+++ main.js
@@ -30,6 +30,56 @@ class Lapiz {
   /**
    * *Instancia = poder acceder a la clase para poder sacar lo que necesite
    */
+  get getMaterial(){
+    return this.#material
+  }
+  /**
+   * ? get es para llamar o obtener y set es para modificar o actualizaar
+   */
+  set setMaterial(nuevoMaterial){
+    this.#material = nuevoMaterial
+  }
+  get getMina(){
+    return this.#mina
+  }
+  set setMina(nuevaMina){
+    this.#mina = nuevaMina
+  }
+  get getColor(){
+    return this.color
+  }
+  set setColor(nuevoColor){
+    this.color = nuevoColor
+  }
+  get getTamano(){
+    return this.tamano
+  }
+  set setTamano(nuevoTamaño){
+    this.tamano = nuevoTamaño
+  }
 }
-let lapiz1 = new Lapiz({tamano : "5cm"});
-console.log(lapiz1);
+// let lapiz1 = new Lapiz({tamano : "5cm"});
+// console.log(lapiz1);
+/**
+ * ! la clase boligrafo hereda las propiedades de la clase lapíz
+ * 
+ * ! el constructor tambien  puede modifica lo que tenga en la clase heredada
+ */
+class boligrafo extends Lapiz{
+  constructor({mina="metalica"}){
+    /**
+     * * super obtiene los parametros del constructor padre
+     */
+    super({mina})
+  }
+  static afilar(p1){
+    return `se afilo la mina de ${p1}`
+  }
+}
+let boli = new boligrafo({})
+boli.setMina ="aluminio"
+console.log(boli.getMina);
+/**
+ * * polimorfismo se pueden cambiar valores de la clase heredada respecto a la padre
+ */
+console.log(boligrafo.afilar(boli.getMina));
